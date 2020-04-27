60b7f17008152e02c9493b70fef78ce009e0deef
Andy <andy.he01@sap.com>
2020-04-27 14:20:44 +0800

    complte chapter1

---
+++ chapter1/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting chapter1 service "
+echo "********************************************************"
+java -jar /usr/local/chapter1/chapter1.jar
