0afb1f4d653747393a521f872a1e13dc79e372b3
Andy <andy.he01@sap.com>
2020-05-04 17:20:43 +0800

    initialize chapter 3

---
+++ chapter3-confsvr/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting chapter3 configuration service "
+echo "********************************************************"
+java -jar /usr/local/chapter3/chapter3-confsvr.jar
---
+++ chapter3-licensing-service/src/main/java/com/sap/andyhecd/microservice/chapter3/licenses/service/OrganizationService.java
@@ -0,0 +1,7 @@
+package com.sap.andyhecd.microservice.chapter3.licenses.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrganizationService {
+}
